f672547113e92f4043c509962fde05a55bb772ca
ra-franco <ramonccastro@hotmail.com>
2025-10-08 22:02:03 -0300

    (08/10) Término da segurança de senhas + segurança de API com token JWT

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,33 +1,38 @@
-import dotenv from 'dotenv'
-dotenv.config() // Carrega as variáveis de ambiente do arquivo .env
+import dotenv from "dotenv";
+dotenv.config(); // Carrega as variáveis de ambiente do arquivo .env
 
-import express, { json, urlencoded } from 'express'
-import cookieParser from 'cookie-parser'
-import logger from 'morgan'
+import express, { json, urlencoded } from "express";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
 
-const app = express()
+const app = express();
 
-import cors from 'cors'
+import cors from "cors";
 
-app.use(cors({
-  origin: process.env.ALLOWED_ORIGINS.split(','),
-  // credentials: true
-}))
+app.use(
+  cors({
+    origin: process.env.ALLOWED_ORIGINS.split(","),
+    // credentials: true
+  })
+);
 
-app.use(logger('dev'))
-app.use(json())
-app.use(urlencoded({ extended: false }))
-app.use(cookieParser())
+app.use(logger("dev"));
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(cookieParser());
 
 /*********** ROTAS DA API **************/
 
-import carsRouter from './routes/cars.js'
-app.use('/cars', carsRouter)
+import carsRouter from "./routes/cars.js";
+app.use("/cars", carsRouter);
 
-import customersRouter from './routes/customers.js'
-app.use('/customers', customersRouter)
+import customersRouter from "./routes/customers.js";
+app.use("/customers", customersRouter);
 
-import usersRouter from './routes/users.js'
-app.use('/users', usersRouter)
+import usersRouter from "./routes/users.js";
+app.use("/users", usersRouter);
 
-export default app
+import auth from "./middleware/auth.js";
+app.use(auth);
+
+export default app;
